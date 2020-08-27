fcde262ae9c8f2a66d5355169ccc7a8435e02fe6
devJRL <dev2sponge@gmail.com>
2020-08-27 18:37:36 +0900

    Get Payload from GraphQL

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,17 +3,33 @@
  */
 import React from 'react';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+
+// GraphQL Query | https://www.apollographql.com/docs/react/data/queries/#executing-a-query
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  return data ? JSON.stringify(data) : null;
+}
 
 // ApolloClient | https://www.apollographql.com/docs/react/get-started/#create-a-client
 const client = new ApolloClient({
-  uri: 'https://localhost:4000', // GraphQL Server Endpoint
+  uri: 'http://localhost:4000', // GraphQL Server Endpoint
   cache: new InMemoryCache()
 });
 
 // ApolloProvider | https://www.apollographql.com/docs/react/get-started/#connect-your-client-to-react
 const Chat = () => { // Somthing Like redux..
-  return <div>I'm a chat window</div>;
+  return <div><Messages user="devJRL" /></div>;
 };
 
 export default () => (
