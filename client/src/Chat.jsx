06c3451bf573196cc9fb11a966a9be5202f3c480
devJRL <dev2sponge@gmail.com>
2020-08-27 19:57:43 +0900

    Align messages(payload) in Shards Container

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -2,7 +2,7 @@
  * Chat widget
  */
 import React from 'react';
-
+import { Container } from 'shards-react';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 
 // GraphQL Query | https://www.apollographql.com/docs/react/data/queries/#executing-a-query
@@ -16,9 +16,53 @@ const GET_MESSAGES = gql`
   }
 `;
 
-const Messages = ({ user }) => {
+const Messages = ({ loginUser }) => {
   const { data } = useQuery(GET_MESSAGES);
-  return data ? JSON.stringify(data) : null;
+  if (!data)
+    return null;
+
+  return (
+    <>
+      {data.messages.map(({ id, content, user: sendUser }) => (
+        <div
+          key={id}
+          style={{
+            display: 'flex',
+            justifyContent: loginUser === sendUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em'
+          }}
+        >
+          {loginUser !== sendUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: "0.5em",
+                border: "2px solid #e5e6ea",
+                borderRadius: 25,
+                textAlign: "center",
+                fontSize: "18pt",
+                paddingTop: 5,
+              }}
+            >
+              {sendUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              maxWidth: "60%",
+              background: loginUser === sendUser ? '#58bf56' : '#e5e6ea',
+              color: loginUser === sendUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em'
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
 // ApolloClient | https://www.apollographql.com/docs/react/get-started/#create-a-client
@@ -29,7 +73,11 @@ const client = new ApolloClient({
 
 // ApolloProvider | https://www.apollographql.com/docs/react/get-started/#connect-your-client-to-react
 const Chat = () => { // Somthing Like redux..
-  return <div><Messages user="devJRL" /></div>;
+  return (
+    <Container>
+      <Messages loginUser="devJRL" />
+    </Container>
+  );
 };
 
 export default () => (
